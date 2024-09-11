360efc5273c580944f7b3e8a9a8575726b8a1777
imjiyeon <yoyt12@naver.com>
2024-09-11 17:14:12 +0900

    ㅇㅇㅇ

--- project04/src/main/java/com/example/demo/domain/MemberRepository.java
+++ project04/src/main/java/com/example/demo/domain/MemberRepository.java
@@ -5,32 +5,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*
-* 가상 리파지토리 만들기
-* */
+//스프링 없이 순수하게 서블릿만 사용하기 위해 가상의 리파지토리 구현
 public class MemberRepository {
 
+	// 회원 정보를 저장할 가상 저장소 (테이블 대신)
     static Map<Integer, Member> store = new HashMap<>();
+    
+    // 회원 번호를 자동으로 생성하기 위한 시퀀스
     static int sequence = 0;
 
-    // 회원 등록
+    // 새로운 회원을 저장소에 추가
     public Member save(Member member) {
-        member.setNo(++sequence);
+        member.setNo(++sequence); //회원 번호를 증가시켜서 설정
         store.put(member.getNo(), member);
         return member;
     }
     
-    // 회원 단건 조회
+    // 회원번호로 저장소에서 회원을 조회
     public Member findById(int id) {
         return store.get(id);
     }
 
-    // 회원 목록 조회
+    // 저장소에 있는 모든 회원을 조회
     public List<Member> findAll() {
+    	// map에 저장된 값들을 collection으로 가져온 후, List로 변환하여 반환
         return new ArrayList<>(store.values());
     }
 
-    // 회원 전체 삭제
+    // 저장소에 있는 모든 회원 정보를 삭제
     public void clearStore() {
         store.clear();
     }
