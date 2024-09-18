5bf9c2bd14f78f403da38b683bfbf4f76234ff76
임지연 <yoyt12@naver.com>
2024-09-18 16:14:26 +0900

    ㅇㅇㅇ

--- project04/src/test/java/com/example/demo/domain/MemberRepositoryTest.java
+++ project04/src/test/java/com/example/demo/domain/MemberRepositoryTest.java
@@ -8,31 +8,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-	// 직접 리파지토리 객체 생성해야함
+	// 스프링을 사용하지 않았기 때문에 직접 리파지토리 객체 생성해야함
 	MemberRepository repository = new MemberRepository();
 	
 	@Test
 	public void 테스트() {
 		
-		//회원 등록
+		// 1.회원 등록
 		Member member1 = new Member(0,"user1","1234");
 		repository.save(member1);
 		
 		Member member2 = new Member(0,"user2","1234");
 		repository.save(member2);
 		
-		//회원 목록 조회
+		// 2.회원 목록 조회
 		List<Member> findList = repository.findAll();
 		System.out.println("회원 목록: ");
 		for(Member member : findList) {
 			System.out.println(member);
 		}
 		
-		//1번째 회원 조회
+		// 3.1번째 회원 조회
 		Member getMember = repository.findById(1);
 		System.out.println("1번째 회원: " + getMember);
 		
-		//회원 전체 삭제
+		// 4.모든 회원 삭제
 		repository.clearStore();
 		List<Member> clearList = repository.findAll();
 		System.out.println("삭제 후 회원 목록: ");
