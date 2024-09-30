a08f06eeeca07a7199574baacf895c3d8fbdfb56
imjiyeon <yoyt12@naver.com>
2024-09-30 11:34:11 +0900

    ㅇㅇㅇ

--- project07/src/test/java/com/example/demo/repository/BoardRepositoryTest3.java
+++ project07/src/test/java/com/example/demo/repository/BoardRepositoryTest3.java
@@ -22,6 +22,7 @@ public class BoardRepositoryTest3 {
 	@Autowired
 	BoardRepository repository;
 
+	// 기존 게시물 테이블을 삭제한 후, 새로 생성하세요
 	@Test
 	void 테스트용게시물등록() {
 		
@@ -37,23 +38,23 @@ public class BoardRepositoryTest3 {
 	@Test
 	void 단일항목검색테스트() {
 
-		// 페이징 조건으로 첫번째 페이지, 데이터 10개 설정
+		// 첫번째 페이지에서 데이터 10개를 가져오는 페이지 조건 설정
 		Pageable pageable = PageRequest.of(0, 10);
 
-		// Q 도메인 클래스 생성
+		// Q도메인 클래스의 인스턴스 생성
 		QBoard qBoard = QBoard.board;
 
-		// builder 객체 생성
+		// builder 객체 생성 (조건을 담을 수 있는 객체)
 		BooleanBuilder builder = new BooleanBuilder();
 
-		// 검색 조건 작성하기
+		// 검색 조건 작성
 		// 작성자에 '둘리'가 포함된 게시물 검색
 		BooleanExpression expression = qBoard.writer.contains("둘리");
 
-		// builder에 조건을 추가
+		// 만들어진 조건을 and로 연결
 		builder.and(expression);
 
-		// 조건을 사용하여 검색
+		// 검색 실행
 		Page<Board> result = repository.findAll(builder, pageable);
 
 		List<Board> list = result.getContent();
@@ -65,7 +66,7 @@ public class BoardRepositoryTest3 {
 	}
 	// SQL에 LIKE 연산자를 사용하며,
 	// ESCAPE '!'는 % 부분을 문자 그대로 처리하기 위한 이스케이프 문자로이다
-	// LIKE 검색 시 키워드에 %가 추가된다
+	// LIKE 검색 시 키워드 앞뒤에 %를 추가하여 검색한다
 
 	@Test
 	void 다중항목검색테스트1() {
@@ -74,14 +75,14 @@ public class BoardRepositoryTest3 {
 		QBoard qBoard = QBoard.board;
 		BooleanBuilder builder = new BooleanBuilder();
 		
-		// 검색 조건 작성하기
-		// 내용에 '안녕'이 포함되고 작성자에 '둘리'가 포함되는 조건 생성
+		// 첫번째 조건: 내용에 '안녕'이 포함되는 게시물
 		BooleanExpression expression = qBoard.content.contains("안녕");
-		builder.and(expression);
-		BooleanExpression expression2 = qBoard.writer.contains("둘리");
+		builder.and(expression); // 조건을 AND로 연결
 		
-		// 조건을 AND로 연결
-		builder.and(expression2);
+		// 두번째 조건: 작성자에 '둘리'가 포함되는 게시물
+		BooleanExpression expression2 = qBoard.writer.contains("둘리");
+		builder.and(expression2); // 조건을 AND로 연결
+		
 		Page<Board> result = repository.findAll(builder, pageable);
 		List<Board> list = result.getContent();
 
@@ -98,10 +99,10 @@ public class BoardRepositoryTest3 {
 		QBoard qBoard = QBoard.board;
 		BooleanBuilder builder = new BooleanBuilder();
 		
-		// 검색 조건 작성하기
-		// 내용에 '안녕하세요'가 포함되거나 작성자에 '또치'가 포한되는 조건 생성
+		// 첫번째 조건: 내용에 '안녕하세요'이 포함되는 게시물
 		BooleanExpression expression = qBoard.content.contains("안녕하세요");
 		
+		// 두번째 조건: 작성자에 '또치'가 포함되는 게시물
 		BooleanExpression expression2 = qBoard.writer.contains("또치");
 		
 		// 조건을 OR로 연결
