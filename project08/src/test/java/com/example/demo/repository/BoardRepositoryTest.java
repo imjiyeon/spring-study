1f83d6b8753d27e4ed40d97a4695cac024087e72
임지연 <yoyt12@naver.com>
2024-10-01 21:08:43 +0900

    ㅇㅇㅇ

--- project08/src/test/java/com/example/demo/repository/BoardRepositoryTest.java
+++ project08/src/test/java/com/example/demo/repository/BoardRepositoryTest.java
@@ -22,23 +22,21 @@ public class BoardRepositoryTest {
 		// 작성자 필드에 사용할 회원 엔티티 생성
 		Member member = Member.builder().id("둘리").build();
 
-		// 회원테이블에 없는 아이디를 사용하면 에러남
+		// 회원 테이블에 존재하지 않는 회원을 작성자로 사용하면 에러남
 		Board board = Board.builder()
 									.title("안녕하세요")
 									.content("안녕하세요")
 									.writer(member)
 									.build();
 
-		repository.save(board);
-		//error
+		repository.save(board); //에러남
 		//com.example.demo.entity.Board.writer -> com.example.demo.entity.Member
 	}
 
 	@Test
 	void 게시물등록() {
 		
-		// 먼저 테이블에 존재하는 회원 정보를 생성
-		// 이때, 회원 엔티티는 PK만 입력하면됨
+		// 테이블에 존재하는 회원 정보로 엔티티 생성 (PK만 필요)
 		Member member = Member.builder().id("user1").build();
 
 		// 작성자 필드에 회원 정보 입력
@@ -49,7 +47,7 @@ public class BoardRepositoryTest {
 									.build();
 		repository.save(board);
 		
-		// 하명의 회원이 게시물을 여러개 작성할 수 있음
+		// 한명의 회원이 여러개의 게시물을 작성할 수 있음
 		Board board2 = Board.builder()
 									.title("반갑습니다")
 									.content("반갑습니다")
@@ -60,11 +58,14 @@ public class BoardRepositoryTest {
 
 	@Test
 	public void 게시물조회() {
-		Optional<Board> optional = repository.findById(1);
-		Board board = optional.get();
-		System.out.println(board); //회원정보가 함께 출력됨
 		
-		// SQL에서 board테이블과 member 테이블과 join 처리됨
+		Optional<Board> optional = repository.findById(1);
+		
+		Board board = optional.get();
+		
+		System.out.println(board); //회원정보도 함께 출력됨
+		
+		// SQL에서 board 테이블과 member 테이블이 join 처리됨
 	}
 
 }
