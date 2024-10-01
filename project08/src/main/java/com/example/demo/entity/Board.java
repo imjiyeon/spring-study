e8e9fc88bc2915f1162c1f75b637ed6c1152f73f
임지연 <yoyt12@naver.com>
2024-10-01 21:10:49 +0900

    ㅇㅇㅇ

--- project08/src/main/java/com/example/demo/entity/Board.java
+++ project08/src/main/java/com/example/demo/entity/Board.java
@@ -22,7 +22,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Board extends BaseEntity { // 등록시간필드와 수정시간필드 상속받기
+public class Board extends BaseEntity { // 등록시간과 수정시간 필드 상속받기
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,14 +37,14 @@ public class Board extends BaseEntity { // 등록시간필드와 수정시간필
 //    @Column(length = 50, nullable = false)
 //    String writer; //작성자
 
-	// 작성자 필드가 외래키로 설정되어, 부모인 Member 테이블의 PK를 참조함
-	@ManyToOne // 관계차수는 1:N
+	// 작성자 필드를 외래키로 설정하여 Member 테이블의 PK를 참조
+	@ManyToOne // 관계차수는 1:N로 설정
 	Member writer; // 작성자
 
 }
 
 // 기존 테이블을 삭제하고, 새로 생성
-// 테이블이 생성될 때 SQL을 확인하면, FK가 설정됨
+// 테이블 생성 시, SQL 쿼리를 보면 FK가 설정된 것을 확인할 수 있음
 // 실제 테이블에서 외래키와 엔티티 관계도 확인
 
 
