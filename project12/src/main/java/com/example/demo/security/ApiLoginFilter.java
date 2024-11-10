e90a7c56cb50ff7214acaa1382c452b5a5122bbf
임지연 <yoyt12@naver.com>
2024-11-10 23:09:17 +0900

    ㅇㅇㅇ

---
+++ project12/src/main/java/com/example/demo/security/ApiLoginFilter.java
@@ -0,0 +1,72 @@
+package com.example.demo.security;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+	protected ApiLoginFilter(String defaultFilterProcessesUrl) {
+		super(defaultFilterProcessesUrl);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+
+		// 요청 메세지에서 바디데이터 가져오기
+		String body = getBody(request);
+		ObjectMapper objectMapper = new ObjectMapper();
+		// json문자열 -> Map객체로 변환
+		HashMap<String, String> map = objectMapper.readValue(body, HashMap.class);
+
+		// 바디데이터에서 아이디와 패스워드 가져오기
+		String id = map.get("id");
+		String password = map.get("password");
+
+		if (id == null) {
+			throw new BadCredentialsException("id cannot be null");
+		}
+
+		return null;
+	}
+
+	// 요청 메세지에서 바디 데이터를 꺼내는 메소드
+	public String getBody(HttpServletRequest request) throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+
+		try {
+			bufferedReader = request.getReader();
+			char[] charBuffer = new char[128];
+			int bytesRead;
+			while ((bytesRead = bufferedReader.read(charBuffer)) != -1) {
+				stringBuilder.append(charBuffer, 0, bytesRead);
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+
+		return stringBuilder.toString();
+	}
+
+}
