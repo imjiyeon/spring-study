5367f855ea2fb874c65f32b1b944a40e8f5dc5e7
imjiyeon <yoyt12@naver.com>
2025-05-19 16:06:45 +0900

    a

---
+++ spring-boot-fileupload-template/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import com.example.demo.entity.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Integer> { 
+}
